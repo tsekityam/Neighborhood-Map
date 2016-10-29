10ccea745bd6a965234c76f2f12eb13cbf0dc1e7
Tse Kit Yam <me@kytse.com>
2016-10-29 23:37:18 +0800

    Show Map of Hong Kong
    We need to use Google Maps JavaScript API to show our neighbourhood map

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 22.3964, lng: 114.1095},  // Set center to Hong Kong
+    zoom: 10  // Set Zoom level to city
+  });
+}
