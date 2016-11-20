29d13a91713117cfc547ce7e33353ee01e81af80
Tse Kit Yam <me@kytse.com>
2016-11-20 11:11:05 +0800

    Add header to indicated where did the file come from

--- js/offcanvas.js
+++ js/offcanvas.js
@@ -1,3 +1,8 @@
+/*
+ * The file is modified from the following Bootstrap example file, released under MIT license.
+ * https://github.com/twbs/bootstrap/blob/master/docs/examples/offcanvas/offcanvas.js
+ */
+
 $(document).ready(function() {
     $('[data-toggle="offcanvas"]').click(function() {
         $('.row-offcanvas').toggleClass('active')
